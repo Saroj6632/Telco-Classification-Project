922f1f663808a60c3a9758ea7d666bd3221fd068
Saroj Duwal <saroj.duwal@gmail.com>
2022-07-13 09:20:05 -0400

    changes made to prepare module

--- prepare.py
+++ prepare.py
@@ -5,6 +5,46 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
+
+def prep_telco(df):
+    '''This function cleans the telcho data by dropping unwanted columns and encoding some columns
+    and also add dummies columns'''
+    
+    df= df.drop_duplicates()
+    
+    # Drop the unwanted columns
+    df = df.drop(columns=['Unnamed: 0','contract_type_id','internet_service_type_id','payment_type_id'])
+    
+    # Convert to correct datatype
+    df.total_charges = df['total_charges'].str.strip().replace('',0).astype(float)
+    
+    # converting categorical variables to numeric
+    df['gender_encoded'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['partner_encoded'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['dependents_encoded'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['phone_service_encoded'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['churn_encoded'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    # Concatenate dummy dataframe to original 
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+
+
 def train_validate_test_split(df, target, seed=123):
     '''
     This function takes in a dataframe, the name of the target variable
@@ -21,82 +61,9 @@ def train_validate_test_split(df, target, seed=123):
                                        random_state=seed,
                                        stratify=train_validate[target])
     return train, validate, test
-
-
-
-
-
-def prep_telco(df):
-    '''This function cleans the telcho data by dropping unwanted columns and replacing some values in columns'''
-    df= df.drop_duplicates()
-    # Drop the unwanted columns
-    df = df.drop(columns=['Unnamed: 0','contract_type_id','internet_service_type_id','payment_type_id'])
-    
-    #replace some values with 'No' in listed columns
-    df.online_security = df['online_security'].str.replace('No internet service', 'No')
-    df.online_backup = df['online_backup'].str.replace('No internet service', 'No')
-    df.device_protection = df['device_protection'].str.replace('No internet service', 'No')
-
-     # Convert to correct datatype
-    df.total_charges = df['total_charges'].str.strip().replace('',0).astype(float)
-    
-
-    return df
     
 
     
-def encode_telco(df):
-#     Family describes the family type based on partners and dependents
 
-    conditions_1 =[
-        (df['partner']=='Yes')& (df['dependents']=='Yes'),
-        (df['partner']=='Yes')& (df['dependents']=='No'),
-        (df['partner']=='No')& (df['dependents']=='Yes'),
-        (df['partner']=='No')& (df['dependents']=='No')]
-    choices_1 = [0,1,2,3]
-    df['family'] = np.select(conditions_1, choices_1)
 
-#Phone services describes whether someone has a phone plan and whether or not they have multiple lines
-
-    conditions_2 =[
-        (df['phone_service']=='Yes')& (df['multiple_lines']=='Yes'),
-        (df['phone_service']=='Yes')& (df['multiple_lines']=='No'),
-        (df['phone_service']=='No')& (df['multiple_lines']== 'No')]
-    choices_2 = [0,1,2]
-    df['phone_services'] = np.select(conditions_2, choices_2)
-
-#Streaming services describes what streaming services someone has.
-
-    conditions_3 =[
-        (df['streaming_tv']=='Yes')& (df['streaming_movies']=='Yes'),
-        (df['streaming_tv']=='Yes')& (df['streaming_movies']=='No'),
-        (df['streaming_tv']=='No')& (df['streaming_movies']=='Yes'),
-        (df['streaming_tv']=='No')& (df['streaming_movies']=='No')]
-    choices_3 = [0,1,2,3]
-    df['streaming_services'] = np.select(conditions_3, choices_3)
-
-#Online_services describes what types of online services someone has.
-
-    conditions_4=[
-        (df['online_security']=='Yes')& (df['online_backup']=='Yes'),
-        (df['online_security']=='Yes')& (df['online_backup']=='No'),
-        (df['online_security']=='No')& (df['online_backup']=='Yes'),
-        (df['online_security']=='No')& (df['online_backup']=='No')]
-    choices_4 = [0,1,2,3]
-    df['online_services'] = np.select(conditions_4, choices_4)
     
-    
-    # encode binary categorical variables into numeric values
-    df['e_device_protection'] = df['device_protection'].map({'Yes':1,'No':0,'No internet service':0})
-    df['e_gender'] = df.gender.map({'Female': 1, 'Male': 0})
-    df['e_paperless_billing'] = df.paperless_billing.map({'Yes':1, 'No':0})
-    df['e_churn'] = df.churn.map({'Yes': 1, 'No': 0})
-    df['etech_support']= df.tech_support.map({'Yes':1, 'No':0,'No internet service':0})
-    df['e_internet_service']= df.internet_service_type.map({'None':0,'DSL':1, 'Fiber optic':2})
-    df['e_payment']= df.payment_type.map({'Electronic check':0,'Mailed check':1,'Bank transfer (automatic)':2,'Credit card (automatic)':3 })
-    df['e_contract']= df.contract_type.map({'One year':1,'Month-to-month':0, 'Two year':2 })
-
-    encoded_df= df[['customer_id','e_churn','e_gender','senior_citizen','e_device_protection','e_paperless_billing','etech_support','e_internet_service','e_payment',
-                    'e_contract','online_services','streaming_services','streaming_services','phone_services','family','monthly_charges','total_charges','tenure']]
-
-    return encoded_df
